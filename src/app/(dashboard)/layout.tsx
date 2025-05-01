8a6c7be2aa45bb8171ee96e9c1992256b953673f
GantangSatria <gantang610@gmail.com>
2025-05-01 07:53:52 +0700

    fix: screen

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,9 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex">
       <ClientSidebarWrapper />
-      <main className="min-h-screen transition-all duration-300]">
+      <main className="flex-1 ml-[280px] md:ml-[300px] lg:ml-[340px] min-h-screen">
         {children}
       </main>
     </div>
